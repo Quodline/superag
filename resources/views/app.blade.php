5c015f32ab1665603ba85d802cee0b424d1e72e4
Ore Adeleye <oreadeleye67@gmail.com>
2022-06-09 08:41:18 +0100

    .gitignore fix

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>SuperAgent Property Experts</title>
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
