ac3eb4803cf1fa1730ea13b8f43bd80e7c168fe3
Ore Adeleye <oreadeleye67@gmail.com>
2022-06-11 12:39:43 +0100

    listing

--- resources/js/Components/UserNav.vue
+++ resources/js/Components/UserNav.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="bg-blue-900 drop-shadow-md text-white px-4 py-8">
     <h1 class="text-2xl">
-        <a href="/products">Superagent Property Experts</a>
+        <a href="/">Superagent Property Experts</a>
     </h1>
 </div>
 </template>
